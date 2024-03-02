2b87747b96ccd1ecc9e75eaf8f2094863d14ec09
Michael Kluge <vollseil@mailbox.org>
2024-03-02 13:56:43 +0100

    tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,138 +1,163 @@
 #include <list>
 #include <Arduino.h>
-//#include <AdaNeoDisplay.h>
-//#include <TetrisGame.h>
-//#include <TaskScheduler.h>
+#include <AdaNeoDisplay.h>
+#include <TetrisGame.h>
+#include <TaskScheduler.h>
 #include <TM1637Display.h>
 
 // Module connection pins (Digital Pins)
-#define CLK 27
-#define DIO 26
+#define CLK_LEFT_LED 27
+#define DIO_LEFT_LED 26
+#define CLK_RIGHT_LED 33
+#define DIO_RIGHT_LED 32
 
-// The amount of time (in milliseconds) between tests
-#define TEST_DELAY 2000
+TM1637Display l8_left(CLK_LEFT_LED, DIO_LEFT_LED);
+TM1637Display l8_right(CLK_RIGHT_LED, DIO_RIGHT_LED);
 
-const uint8_t SEG_DONE[] = {
-    SEG_B | SEG_C | SEG_D | SEG_E | SEG_G,         // d
-    SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F, // O
-    SEG_C | SEG_E | SEG_G,                         // n
-    SEG_A | SEG_D | SEG_E | SEG_F | SEG_G          // E
+struct Button
+{
+  const uint8_t PIN;
+  uint32_t numberKeyPresses;
+  bool pressed;
 };
 
-TM1637Display display(CLK, DIO);
+Button button1 = {22, 0, false};
+
+// variables to keep track of the timing of recent interrupts
+unsigned long button_time = 0;
+unsigned long last_button_time = 0;
+
+void IRAM_ATTR isr()
+{
+  button_time = millis();
+  if (button_time - last_button_time > 250)
+  {
+    button1.numberKeyPresses++;
+    button1.pressed = true;
+    last_button_time = button_time;
+  }
+}
+
+// input pins
+#define DISPLAY_PIN 14
+#define L_JOYSTICK_PIN 22
+#define R_JOYSTICK_PIN 23
+#define U_JOYSTICK_PIN 15
+#define D_JOYSTICK_PIN 2
+#define L_PUSH_PIN 4
+#define R_PUSH_PIN 16
+#define BUTTON_LEFT_LED 1
+#define BUTTON_RIGHT_LED 17
+
+std::list<uint8_t> input_pins = {
+    L_JOYSTICK_PIN,
+    R_JOYSTICK_PIN,
+    U_JOYSTICK_PIN,
+    D_JOYSTICK_PIN,
+    L_PUSH_PIN,
+    R_PUSH_PIN,
+};
+std::list<uint8_t> output_pins = {
+    DISPLAY_PIN,
+    BUTTON_LEFT_LED,
+    BUTTON_RIGHT_LED,
+};
+#define KEY_UPDATE(pin, field)           \
+  {                                      \
+    bool pressed = digitalRead(pin);     \
+    if (pressed != old_key_state.field)  \
+    {                                    \
+      current_key_state.field = pressed; \
+    }                                    \
+  }
+
+#define PIXELS_X 8
+#define PIXELS_Y 12
+
+auto native_display = Adafruit_NeoPixel(PIXELS_X * PIXELS_Y, DISPLAY_PIN, NEO_GRB + NEO_KHZ800);
+AdaNeoDisplay display = AdaNeoDisplay(native_display, PIXELS_X, PIXELS_Y);
+TetrisGame tetris = TetrisGame(display, PIXELS_X, PIXELS_Y);
+
+Keys old_key_state;
+Keys current_key_state;
+
+void tetrisThread();
+void leftLEDThread();
+Scheduler runner;
+Task tetrisTask(1000, TASK_FOREVER, &tetrisThread);
+Task lefLedTask(1000, TASK_FOREVER, &leftLEDThread);
+
+void leftLEDThread()
+{
+  static int counter = 0;
+  l8_left.showNumberDec(counter);
+  counter++;
+}
+
+void tetrisThread()
+{
+  tetris.animate(current_key_state);
+  display.show();
+  old_key_state = current_key_state;
+}
+
+void initPins()
+{
+  for (auto pin : input_pins)
+  {
+    pinMode(pin, INPUT_PULLUP);
+  }
+  for (auto pin : output_pins)
+  {
+    pinMode(pin, OUTPUT);
+  }
+}
 
 void setup()
 {
-  // Clear the display:
-  display.clear();
-  delay(1000);
+  display.start();
+  randomSeed(analogRead(0)); // Seed the random number generator with an analog reading
+  initPins();
+  l8_left.clear();
+  l8_right.clear();
+  l8_right.showNumberDec(1234);
+  runner.addTask(tetrisTask);
+  runner.addTask(lefLedTask);
+  //  Serial.begin(115200);
+  digitalWrite(BUTTON_LEFT_LED,1);
+  digitalWrite(BUTTON_RIGHT_LED,1);
+  pinMode(button1.PIN, INPUT_PULLUP);
+  attachInterrupt(button1.PIN, isr, FALLING);
+}
+
+/**
+ * updates keys and sets a new status if the input
+ * is different from the known old state that was used
+ * during the last animation sequence
+ * thus: the anmiation task has to copy current_key_state
+ *       to old_key_state after the animation
+ */
+void readKeys()
+{
+  KEY_UPDATE(L_JOYSTICK_PIN, left_joy);
+  KEY_UPDATE(R_JOYSTICK_PIN, right_joy);
+  KEY_UPDATE(U_JOYSTICK_PIN, up_joy);
+  KEY_UPDATE(D_JOYSTICK_PIN, down_joy);
+  KEY_UPDATE(L_PUSH_PIN, left_push);
+  KEY_UPDATE(R_PUSH_PIN, right_push);
 }
 
 void loop()
 {
-  int k;
-  uint8_t data[] = {0xff, 0xff, 0xff, 0xff};
-  uint8_t blank[] = {0x00, 0x00, 0x00, 0x00};
-  display.setBrightness(0x0f);
-
-  // All segments on
-  display.setSegments(data);
-  delay(TEST_DELAY);
-
-  // Selectively set different digits
-  data[0] = display.encodeDigit(0);
-  data[1] = display.encodeDigit(1);
-  data[2] = display.encodeDigit(2);
-  data[3] = display.encodeDigit(3);
-  display.setSegments(data);
-  delay(TEST_DELAY);
-
-  /*
-  for(k = 3; k >= 0; k--) {
-  display.setSegments(data, 1, k);
-  delay(TEST_DELAY);
-  }
-  */
-
-  display.clear();
-  display.setSegments(data + 2, 2, 2);
-  delay(TEST_DELAY);
-
-  display.clear();
-  display.setSegments(data + 2, 2, 1);
-  delay(TEST_DELAY);
-
-  display.clear();
-  display.setSegments(data + 1, 3, 1);
-  delay(TEST_DELAY);
-
-  // Show decimal numbers with/without leading zeros
-  display.showNumberDec(0, false); // Expect: ___0
-  delay(TEST_DELAY);
-  display.showNumberDec(0, true); // Expect: 0000
-  delay(TEST_DELAY);
-  display.showNumberDec(1, false); // Expect: ___1
-  delay(TEST_DELAY);
-  display.showNumberDec(1, true); // Expect: 0001
-  delay(TEST_DELAY);
-  display.showNumberDec(301, false); // Expect: _301
-  delay(TEST_DELAY);
-  display.showNumberDec(301, true); // Expect: 0301
-  delay(TEST_DELAY);
-  display.clear();
-  display.showNumberDec(14, false, 2, 1); // Expect: _14_
-  delay(TEST_DELAY);
-  display.clear();
-  display.showNumberDec(4, true, 2, 2); // Expect: __04
-  delay(TEST_DELAY);
-  display.showNumberDec(-1, false); // Expect: __-1
-  delay(TEST_DELAY);
-  display.showNumberDec(-12); // Expect: _-12
-  delay(TEST_DELAY);
-  display.showNumberDec(-999); // Expect: -999
-  delay(TEST_DELAY);
-  display.clear();
-  display.showNumberDec(-5, false, 3, 0); // Expect: _-5_
-  delay(TEST_DELAY);
-  display.showNumberHexEx(0xf1af); // Expect: f1Af
-  delay(TEST_DELAY);
-  display.showNumberHexEx(0x2c); // Expect: __2C
-  delay(TEST_DELAY);
-  display.showNumberHexEx(0xd1, 0, true); // Expect: 00d1
-  delay(TEST_DELAY);
-  display.clear();
-  display.showNumberHexEx(0xd1, 0, true, 2); // Expect: d1__
-  delay(TEST_DELAY);
-
-  // Run through all the dots
-  for (k = 0; k <= 4; k++)
+  static int num_pressed = 0;
+  if (button1.pressed)
   {
-    display.showNumberDecEx(0, (0x80 >> k), true);
-    delay(TEST_DELAY);
+    num_pressed++;
+    l8_right.showNumberDec(num_pressed);
+    button1.pressed = false;
   }
-
-  // Brightness Test
-  for (k = 0; k < 4; k++)
-    data[k] = 0xff;
-  for (k = 0; k < 7; k++)
-  {
-    display.setBrightness(k);
-    display.setSegments(data);
-    delay(TEST_DELAY);
-  }
-
-  // On/Off test
-  for (k = 0; k < 4; k++)
-  {
-    display.setBrightness(7, false); // Turn off
-    display.setSegments(data);
-    delay(TEST_DELAY);
-    display.setBrightness(7, true); // Turn on
-    display.setSegments(data);
-    delay(TEST_DELAY);
-  }
-
-  // Done!
-  display.setSegments(SEG_DONE);
+  // first: update the key status
+  readKeys();
+  // the run all animations
+  runner.execute();
 }
-
