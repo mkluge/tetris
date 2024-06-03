befd11365c712ffd1fa16ce58beb3e1f2e7b904b
Carl-Philip Hänsch <carl-philip.haensch@dzastro.de>
2024-06-03 19:28:17 +0200

    Buntes Menü

--- src/Laufschrift.h
+++ src/Laufschrift.h
@@ -9,22 +9,32 @@ inline int MIN(int a, int b) { return((a) < (b) ? a : b); }
 class Laufschrift {
     LEDDisplay &display;
     const char *image[12] = {
-    "  .....    .  .......    ......                                                            ",
-    " .......  ..  ........  ........                                                           ",
-    " ..       ..  ..    ..  ..                                                                 ",
-    " ..       ..  ..    ..  .......                                                            ",
-    " ..       ..  ..    ..   .......                                                           ",
-    " ..       ..  ..    ..        ..                                                           ",
-    " ..       ..  ..    ..        ..                                                           ",
-    " .......  ..  ........   .......                                                           ",
-    "  .....   .   .......   .......                                                            "};
+    "  .....    .  .......    ......          -                                               ..      ",
+    " .......  ..  ........  ........         --                                              ..      ",
+    " ..       ..  ..    ..  ..               -                            .....          -   ..      ",
+    " ..       ..  ..    ..  .......        .     .           ..           .   .          --  ..      ",
+    " ..       ..  ..    ..   .......       ..   ...         .  .          .                          ",
+    " ..       ..  ..    ..        ..       ..   ...        .    .         .                          ",
+    " ..       ..  ..    ..        ..       ...  ...        .    .         .   -                      ",
+    " .......  ..  ........   .......       .... ...       .      .        .                  ..      ",
+    "  .....   .   .......   .......        .... ...       .      -       ..                  ..      "};
+    // CIDS Tetris:39 Pong:54 Snake:69 Flappybird:82
     const char *image2[3] = {
     " -    | ",
     "-      |",
     " -    | "};
+    const struct{char* name; int start; } games[4] = {
+        {"Tetris", 40},
+        {"Pong", 55},
+        {"Snake", 69},
+        {"Flappybird", 85},
+    };
     int pos = 0;
     int pos2 = 0;
+    int targetgame = -1;
+    int idle = 0;
     int sz = strlen(image[0]);
+    const int scrollduration = 4;
 public:
 
 
@@ -70,7 +80,7 @@ public:
     void run() {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 8; j++) {
-                display.setPixel(j, 11-i, getColor(image[i][(pos/4 + j) % sz]));
+                display.setPixel(j, 11-i, getColor(image[i][(pos/scrollduration + j) % sz]));
             }
         }
         for (int i = 0; i < 3; i++) {
@@ -80,8 +90,51 @@ public:
             }
         }
         display.show();
-        l8_left.showNumberDec(pos);
-        pos = (pos + 1) % (4*sz);
-        pos2++;
+        pos2++; // light animation
+        //l8_left.showNumberDec(pos);
+
+        // animation logic
+        if (targetgame == -1) {
+            // scroll mode
+            pos = (pos + 1) % (scrollduration*sz);
+        } else {
+            // select mode
+            if (pos < games[targetgame].start*scrollduration) {
+                pos += scrollduration;
+            }
+            if (pos > games[targetgame].start*scrollduration) {
+                pos -= scrollduration;
+            }
+        }
+        idle++;
+        if (idle > 500 && targetgame != -1) {
+            idle = 0;
+            targetgame = -1;
+        }
+
+        // key press logic
+        auto events = keyboard.toggled();
+        int ngames = sizeof(games) / sizeof(games[0]);
+        for( const auto &key: events) {
+            l8_left.showNumberDec(key.first);
+            l8_right.showNumberDec(key.second);
+            if (key.first == 18 && key.second) {
+                if (targetgame == -1) {
+                    targetgame = ngames - 1;
+                } else if (targetgame > 0) {
+                    targetgame--;
+                }
+                idle = 0;
+            }
+            if (key.first == 21 && key.second) {
+                if (targetgame == -1) {
+                    targetgame = 0;
+                } else if (targetgame < ngames - 1) {
+                    targetgame++;
+                }
+                idle = 0;
+            }
+        }
+        //l8_right.showNumberDec(events.size());
     }
 };
