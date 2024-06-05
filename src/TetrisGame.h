ece6360334cb1781d299c80a402e5f440f140e85
Michael Kluge <michael.kluge@tu-dresden.de>
2024-06-05 08:05:24 +0200

    switch to header only

--- src/TetrisGame.h
+++ src/TetrisGame.h
@@ -1,37 +1,138 @@
 #ifndef TETRISGAME_H
 #define TETRISGAME_H
 
-#include <list>
-#include <functional>
-#include <TetrisPiece.h>
-#include <LEDDisplay.h>
-#include <Keyboard.h>
 #include <GameInterface.h>
+#include <Keyboard.h>
+#include <LEDDisplay.h>
 #include <TM1637Display.h>
+#include <TetrisPiece.h>
+#include <functional>
+#include <list>
 
 #pragma once
 
-class TetrisGame : public GameInterface
-{
+class TetrisGame : public GameInterface {
 public:
-    TetrisGame(LEDDisplay &display, int width, int height, TM1637Display& point_leds);
-    ~TetrisGame();
+  TetrisGame(LEDDisplay &display, int width, int height,
+             TM1637Display &point_leds)
+      : display(display), width(width), height(height), point_leds(point_leds) {
+  }
 
-    void start();
-    void stop();
-    void onKey(const Keyboard::key_state_map_t &keys);
-    void animate();
+  ~TetrisGame() {}
+
+  void start() {
+    floor = new TetrisPiece(display);
+    floor->clearPixels();
+    falling = new TetrisPiece(display);
+    timer_interval = 700;
+    points = 0;
+    last_millis = millis();
+  }
+
+  void stop() {}
+
+  void onKey(const Keyboard::key_state_map_t &keys) {
+    int x_translate = 0;
+    for (const auto &entry : keys) {
+      const int key = entry.first;
+      const int presses = entry.second;
+      if (key == L_JOYSTICK_PIN && presses > 0) {
+        if (falling->canTranslate(-1, 0, floor)) {
+          falling->unpaint();
+          falling->useCachePixels();
+          falling->paint();
+        }
+      }
+      if (key == R_JOYSTICK_PIN && presses > 0) {
+        if (falling->canTranslate(1, 0, floor)) {
+          falling->unpaint();
+          falling->useCachePixels();
+          falling->paint();
+        }
+      }
+      if (key == D_JOYSTICK_PIN && presses > 0) {
+        // move element down as far as possible
+        while (falling->canTranslate(0, -1, floor)) {
+          falling->unpaint();
+          falling->useCachePixels();
+          falling->paint();
+        }
+      }
+      if ((key == L_PUSH_PIN || key == R_PUSH_PIN) && presses > 0) {
+        if (falling->canRotate(floor)) {
+          falling->unpaint();
+          falling->useCachePixels();
+          falling->paint();
+        }
+      }
+    }
+    display.show();
+  }
+
+  /**
+   * animate is called as often as possible
+   */
+  void animate() {
+    // get the current millis, get the diff and
+    // figure out, if we need to do a step
+    unsigned millies = millis();
+    auto diff = millies - last_millis;
+    if (diff >= timer_interval) {
+      // OK, make a step
+      // let element fall 1 step
+      // remove old pixels
+      falling->unpaint();
+      // do we hit the floor or the ground
+      bool canProceed = falling->canTranslate(0, -1, floor);
+      if (!canProceed) {
+        // check if game is over because highest
+        // line is at top row
+        for (const auto &pixel : falling->getPixels()) {
+          if (pixel.y == (height - 1)) {
+            // game finished
+            display.clear();
+            start();
+          }
+        }
+        // merge falling into floor
+        floor->addPixels(falling->getPixels());
+        // check for complete rows
+        // future: funny animation for full row removal
+        // and remove complete rows, get points
+        int numDeletedRows = floor->removeFullLines();
+        points += numDeletedRows;
+        point_leds.showNumberDec(points);
+        // draw new floor
+        floor->paint();
+        // create new falling piece
+        falling = new TetrisPiece(display);
+      } else {
+        // we already checked whether there is
+        // space below so just go one down
+        falling->useCachePixels();
+        falling->paint();
+      }
+      // store time of last action
+      last_millis = millies;
+      // decrease delay between steps
+      // this increases game speed
+      if (timer_interval > 100) {
+        timer_interval--;
+      }
+      display.show();
+    }
+  }
 
 private:
-    LEDDisplay &display;
-    int width;
-    int height;
-    TM1637Display& point_leds;
-    int points;
-    TetrisPiece *falling;
-    TetrisPiece *floor;
-    unsigned long timer_interval;
-    unsigned long last_millis;
+  LEDDisplay &display;
+  int width;
+  int height;
+  TM1637Display &point_leds;
+  int points;
+  TetrisPiece *falling;
+  TetrisPiece *floor;
+  unsigned long timer_interval;
+  unsigned long last_millis;
 };
 
 #endif
--- src/TetrisPiece.h
+++ src/TetrisPiece.h
@@ -1,18 +1,76 @@
 #ifndef TETRISPIECE_H
 #define TETRISPIECE_H
 
+#include "esp_random.h"
 #include <Animation.h>
+#include <Arduino.h>
 #include <Basics.h>
+#include <stdlib.h>
 #include <vector>
 
 #pragma once
 
 class TetrisPiece : public Animation {
 public:
-  TetrisPiece(LEDDisplay &display);
-  ~TetrisPiece();
-  int removeFullLines();
-  void removeLine(int line);
+  TetrisPiece(LEDDisplay &display) : Animation(display) {
+    int avail_pieces = pieceTemplates.size();
+    variant = esp_random() % avail_pieces;
+    // add pixels
+    addPixels(pieceTemplates[variant]);
+    // calculate offset_x and offset_y so that piece
+    // starts at the top of the screen and in the middle
+    auto bBox = boundingBox();
+    offset_x = (display_width / 2) - ((bBox.end_x - bBox.start_x) / 2);
+    offset_y = display.height() - bBox.end_y;
+    Serial.printf("offset x: %d\n", offset_x);
+    Serial.printf("offset y: %d\n", offset_y);
+  }
+
+  ~TetrisPiece() {}
+
+  int removeFullLines() {
+    int removed_lines = 0;
+    // from the bottom up remove all full lines
+    for (int line = display_height - 1; line >= 0; line--) {
+      // count pieces in all lines
+      int piecesPerLine[display_height];
+      for (const auto &pixel : internal_pixels) {
+        auto x = offset_x + pixel.x;
+        auto y = offset_y + pixel.y;
+
+        if (x < 0 || x >= display_width || y < 0 || y >= display_width) {
+          // shall not happen, but ...
+          continue;
+        }
+        piecesPerLine[y]++;
+      }
+      if (piecesPerLine[line] == (display_width - 1)) {
+        removed_lines++;
+        removeLine(line);
+        // we need to check the same line again
+        // FIXME
+        line++;
+      }
+    }
+    return removed_lines;
+  }
+
+  /**
+   * all lines bigger than the selected one is moved one up
+   */
+  void removeLine(int line) {
+    std::list<Pixel> new_pixels;
+    for (auto &pixel : internal_pixels) {
+      auto real_y = offset_y + pixel.y;
+      if (real_y < line) {
+        new_pixels.push_back(pixel);
+      } else if (real_y > line) {
+        pixel.y--;
+        new_pixels.push_back(pixel);
+      }
+    }
+    internal_pixels = new_pixels;
+  }
 
   const std::vector<PixelList> pieceTemplates = {
       // yellow rectangle
@@ -20,16 +78,17 @@ public:
       // blue bar
       {{0, 0, TBLUE}, {0, 1, TBLUE}, {0, 2, TBLUE}, {0, 3, TBLUE}},
       // red s
-      {{-1,-1,TRED},{0,-1,TRED},{0,0,TRED},{1,0,TRED}},
+      {{-1, -1, TRED}, {0, -1, TRED}, {0, 0, TRED}, {1, 0, TRED}},
       // green z
-      {{-1,0,TGREEN},{0,0,TGREEN},{0,-1,TGREEN},{1,-1,TGREEN}},
+      {{-1, 0, TGREEN}, {0, 0, TGREEN}, {0, -1, TGREEN}, {1, -1, TGREEN}},
       // orange l
-      {{0,-1,TORANGE},{-1,-1,TORANGE},{-1,0,TORANGE},{-1,1,TORANGE}},
+      {{0, -1, TORANGE}, {-1, -1, TORANGE}, {-1, 0, TORANGE}, {-1, 1, TORANGE}},
       // pink j
-      {{0,1,TPINK},{0,0,TPINK},{0,-1,TPINK},{-1,-1,TPINK}},
+      {{0, 1, TPINK}, {0, 0, TPINK}, {0, -1, TPINK}, {-1, -1, TPINK}},
       // brown t
-      {{-1,0,TBROWN},{0,0,TBROWN},{1,0,TBROWN},{0,-1,TBROWN}},
+      {{-1, 0, TBROWN}, {0, 0, TBROWN}, {1, 0, TBROWN}, {0, -1, TBROWN}},
   };
+
 private:
   int variant;
 };
